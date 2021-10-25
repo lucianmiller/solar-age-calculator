cc43a9b9ddb83761bda1b5135c3d5e1c3a1c8e84
Lucian Miller <lucian.miller185@gmail.com>
2021-10-25 11:04:57 -0700

    add failing test for AgeCalculator

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -2,8 +2,17 @@ import AgeCalculator from "../src/calculator";
 
 describe('AgeCalculator', () => {
 
-  test('should creat a AgeCalculator object with users age on Earth', () => {
+  test('should creat an AgeCalculator object with users age on Earth', () => {
     const userAge = new AgeCalculator(25);
     expect(userAge.earthAge).toEqual(25);
+  });
+
+  test('should creat an AgeCalculator object with users age on Earth and places to store age on other planets', () => {
+    const userAge = new AgeCalculator(25);
+    expect(userAge.earthAge).toEqual(25);
+    expect(userAge.mercuryAge).toEqual(0);
+    expect(userAge.venusAge).toEqual(0);
+    expect(userAge.marsAge).toEqual(0);
+    expect(userAge.jupiterAge).toEqual(0);
   });
 });
