9766811e217c46164ecbbe793d2ddac86823f6fa
Lucian Miller <lucian.miller185@gmail.com>
2021-10-25 11:28:35 -0700

    add veriables for different planets

--- src/calculator.js
+++ src/calculator.js
@@ -1,5 +1,9 @@
 export default class AgeCalculator {
   constructor(userAge) {
     this.earthAge = userAge;
+    this.mercuryAge = 0;
+    this.venusAge = 0;
+    this.marsAge = 0;
+    this.jupiterAge = 0;
   }
 }
